006f5a993b38335ddaf574c67b5c97e8975263b5
Aslak Hollund <aslakhol@gmail.com>
2018-09-18 17:48:46 +0200

    fix merge

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
-
-
+import './style/Dropdown.css';
 import registerServiceWorker from './utils/registerServiceWorker';
-import Cardmenu from './components/Cardmenu';
+import Menu from './components/Menu';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faAngleUp, faAngleDown, faCheck } from '@fortawesome/free-solid-svg-icons'
+import CardDisplay from './components/cardDisplay';
+import './style/index.css';
+import './style/cardDisplay.css';
+import MediaHandler from './components/MediaHandler';
 
+library.add(faAngleUp, faAngleDown, faCheck);
+const App = () => {
+    return (
+        <div>
+            <MediaHandler />
+        </div>
+    );
+};
 
-ReactDOM.render(<Cardmenu />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
