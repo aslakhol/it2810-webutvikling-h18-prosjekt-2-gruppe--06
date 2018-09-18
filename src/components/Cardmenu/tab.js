b0649a44d97d6a5d2b38094377d9e08556185492
Adrian <adrianhthompson@gmail.com>
2018-09-18 13:51:27 +0200

    added functionality for clicking 'tab'

--- src/components/Cardmenu/tab.js
+++ src/components/Cardmenu/tab.js
@@ -3,14 +3,31 @@ import React, { Component } from 'react';
 class Tab extends Component{
 
 
-    render(){
+    onClick = () => {
+        const {label, onClick} = this.props;
+        onClick(label);
+    }
 
+    render(){
+        const{onClick, props:{
+            activeTab, label,
+        },
+    } = this;
+
+    let className = "list-item";
+
+    if(activeTab === label){
+        className += "active-tab";
+    }
 
         return(
+            <li className={className} onClick={onClick}>
+            {label}
+            
+            </li>
 
 
-
-        )
+        );
     }
 }
 
--- src/components/Cardmenu/tabs.js
+++ src/components/Cardmenu/tabs.js
@@ -7,15 +7,43 @@ class Tabs extends Component {
     constructor(props){
         super(props);
 
+        this.state= {
+            activeTab: this.props.children[0],
+        }
+
+    }
+
+
+    clickedTab = (tab) => {
+        this.setState({ activeTab: tab});
     }
 
 
     render(){
-
-
-
+        const{
+            clickedTab,
+            props: {
+                children,
+            },
+            state: {
+                activeTab,
+            }
+        } = this;
 
         return(
+            <div className="divForTabsAndContent">
+                <ol className="tabs">
+                    {children.map((child) => {
+                        const {label} = child.props;
+                        return(
+                            <Tab activeTab={activeTab} key ={label} label={label} onClick={clickedTab} />
+                        )
+                    })}
+                </ol>
+                <div className="content">
+                    
+                </div>
+            </div>
 
 
         );
