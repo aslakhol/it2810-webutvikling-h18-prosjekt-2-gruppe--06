c833dde9f96b5358fef535982c50b3bfcc84e800
Aslak Hollund <aslakhol@gmail.com>
2018-09-19 17:18:18 +0200

    lots of refactor

---
+++ src/components/dropDownMenu/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+class DropDownMenu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listOpen: false,
+        }
+    }
+     
+    handleClickOutside() {
+        this.setState({
+            listOpen: false
+        })
+    }
+
+    toggleList() {
+        this.setState(prevState => ({
+            listOpen: !prevState.listOpen
+        }))
+    }
+
+    render() {
+        const { toggleItem, title, categories, selected } = this.props;
+        const { listOpen } = this.state;
+        return (
+            <div className="dd-wrapper">
+                <div className="dd-header" onClick={ () => this.toggleList() }>
+                    <div className="dd-header-title">{ title }</div>
+                    {listOpen
+                        ? <FontAwesomeIcon icon="angle-up" size="2x" />
+                        : <FontAwesomeIcon icon="angle-down" size="2x" />
+                    }
+                </div>
+                {listOpen && <ul className="dd-list">
+                    {categories.map((item) => (
+                        <li className="dd-list-item" key={ item.name } onClick={ () => toggleItem(item.id, item.key) }>
+                            { item.name }
+                            { item.id === selected && <FontAwesomeIcon className="check" icon="check" /> }
+                        </li>
+                    ))}
+                </ul>}
+            </div>
+        )
+    }
+}
+
+export default DropDownMenu;
---
+++ src/utils/initialState.js
@@ -0,0 +1,81 @@
+const group = {
+        categories: [
+            {
+                name: "comitee",
+                items: [],
+                id: 0,
+                key: "group",
+            },
+            {
+                name: "subcomitee",
+                items: [],
+                id: 1,
+                key: "group",
+            },
+            {
+                name: "interestgroup",
+                items: [],
+                id: 2,
+                key: "group",
+            },
+        ],
+        selected: 0,
+    }
+
+const sound = {
+        categories: [
+            {
+                name: "lazer",
+                items: [],
+                id: 0,
+                key: "sound",
+                directory:"lazer"
+
+            },
+            {
+                name: "powerup",
+                items: [],
+                id: 1,
+                key: "sound",
+                directory: "powerup"
+            },
+            {
+                name: "pickup-coin",
+                items: [],
+                id: 2,
+                key: "sound",
+                directory:"coin"
+            },
+        ],
+        selected: 0,
+    }
+
+const icon = {
+        categories: [
+            {
+                name: "Arrangerende",
+                items: [],
+                id: 0,
+                key: "icon",
+                directory:"arranging",
+
+            },
+            {
+                name: "Støttende",
+                items: [],
+                id: 1,
+                key: "icon",
+                directory: "supporting",
+            },
+            {
+                name: "online-logoer",
+                items: [],
+                id: 2,
+                key: "icon",
+                directory: "online"
+            },
+        ],
+        selected: 0,
+    }
+
+export { icon, sound, group } 
