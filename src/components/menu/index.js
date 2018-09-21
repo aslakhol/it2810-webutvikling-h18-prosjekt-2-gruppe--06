405570a9ab5cacf3939e79f544da0c59439e4bf6
Aslak Hollund <aslakhol@gmail.com>
2018-09-21 15:57:03 +0200

    remove reduntant import

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import DropDownMenu from '../dropDownMenu'
+import React from 'react';
+import DropDownMenu from '../dropDownMenu';
 
 const Menu = (props) => {
     return (
